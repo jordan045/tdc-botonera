9f015e9d88349399a115f402c018d9e949b04491
AugustoRiedinger <riedinger.augusto@gmail.com>
2024-08-02 14:01:53 -0300

    source y header overlay_360_0001

--- overlay_360_0001.cpp
+++ overlay_360_0001.cpp
@@ -1,14 +1,15 @@
 #include "overlay_360_0001.h"
 #include "ui_overlay_360_0001.h"
 
-overlay_360_0001::overlay_360_0001(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::overlay_360_0001)
+OVERLAY_360_0001::OVERLAY_360_0001(Botonera *b) :
+    ui(new Ui::OVERLAY_360_0001)
 {
     ui->setupUi(this);
+    miBotonera = b;
+    initiate();
 }
 
-overlay_360_0001::~overlay_360_0001()
+OVERLAY_360_0001::~OVERLAY_360_0001()
 {
     delete ui;
 }
--- overlay_360_0001.h
+++ overlay_360_0001.h
@@ -1,22 +1,21 @@
 #ifndef OVERLAY_360_0001_H
 #define OVERLAY_360_0001_H
 
+#include "qek.h"
 #include <QWidget>
 
 namespace Ui {
-class overlay_360_0001;
+class OVERLAY_360_0001;
 }
 
-class overlay_360_0001 : public QWidget
+class OVERLAY_360_0001 : public Qek
 {
-    Q_OBJECT
-
 public:
-    explicit overlay_360_0001(QWidget *parent = nullptr);
-    ~overlay_360_0001();
+    explicit OVERLAY_360_0001(Botonera *b);
+    ~OVERLAY_360_0001();
 
 private:
-    Ui::overlay_360_0001 *ui;
+    Ui::OVERLAY_360_0001 *ui;
 };
 
 #endif // OVERLAY_360_0001_H
