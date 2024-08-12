43dd15c8f02ca042d4f5b28ca2f240f155e8873e
Mario Córdoba <cord0388@gmail.com>
2024-08-11 21:15:31 -0300

    agregando testing
    Primer commit con esqueleto inicial para el testing

--- tdc-botonera.pro
+++ tdc-botonera.pro
@@ -1,5 +1,6 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
-    botonera/juego.pro
+    botonera/juego.pro \
+    testingBotonera
 
---
+++ testingBotonera/tst_formatconcentratortest.cpp
@@ -0,0 +1,31 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class formatConcentratorTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    formatConcentratorTest();
+    ~formatConcentratorTest();
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void test_case01();
+};
+
+formatConcentratorTest::formatConcentratorTest() {}
+
+formatConcentratorTest::~formatConcentratorTest() {}
+
+void formatConcentratorTest::initTestCase() {}
+
+void formatConcentratorTest::cleanupTestCase() {}
+
+void formatConcentratorTest::test_case01() {}
+
+QTEST_APPLESS_MAIN(formatConcentratorTest)
+
+#include "tst_formatconcentratortest.moc"
