8d67112fbd52ce5d4238c11ccb8be869b6436753
Mario Córdoba <cord0388@gmail.com>
2024-08-30 18:14:30 -0300

    added initial test case

--- testingBotonera/tst_formatconcentratortest.cpp
+++ testingBotonera/tst_formatconcentratortest.cpp
@@ -29,23 +29,50 @@ void formatConcentratorTest::initTestCase() {}
 void formatConcentratorTest::cleanupTestCase() {}
 
 void formatConcentratorTest::test_case01_data(){
-    QTest::addColumn<QString>("overlay");
     QTest::addColumn<QString>("range");
+    QTest::addColumn<QString>("displaySelection");
+    QTest::addColumn<QString>("threatAssessment");
+    QTest::addColumn<QString>("center");
+    QTest::addColumn<QString>("displayMode");
+    QTest::addColumn<QString>("qek");
+    QTest::addColumn<QString>("icm");
+    QTest::addColumn<QString>("overlay");
     QTest::addColumn<QBitArray>("result");
 
-    QTest::newRow("") << "0001" << "RANGE2" << BitArrayUtils::qBitArrayfromString("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    QTest::newRow("")   << "RANGE2"
+                        << "air"
+                        << "12_sec"
+                        << "CU_OR_OFF_CENT"
+                        << "RR"
+                        << "QEK_20"
+                        << "1"
+                        << "0001"
+                        << BitArrayUtils::qBitArrayfromString("000000000000100000001000000000001000000000000001000000000000000000000000000000000000000000001000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000");
 }
 
 void formatConcentratorTest::test_case01() {
-    QFETCH(QString, overlay);
     QFETCH(QString, range);
+    QFETCH(QString, displaySelection);
+    QFETCH(QString, threatAssessment);
+    QFETCH(QString, center);
+    QFETCH(QString, displayMode);
+    QFETCH(QString, qek);
+    QFETCH(QString, icm);
+    QFETCH(QString, overlay);
     QFETCH(QBitArray, result);
 
     FormatConcentrator* concentrator = new FormatConcentrator();
     StubEstado* stubEstado = new StubEstado();
 
-    stubEstado->setOverlay(overlay);
     stubEstado->setRange(range);
+    stubEstado->setDisplaySelection(displaySelection);
+    stubEstado->setThreat(threatAssessment);
+    stubEstado->setCenter(center);
+    stubEstado->setDisplayMode(displayMode);
+    stubEstado->setQEK(qek);
+    stubEstado->setICM(icm);
+    stubEstado->setOverlay(overlay);
+
     QBitArray* localTest = concentrator->getMessage(stubEstado);
 
     QCOMPARE(*localTest, result);
