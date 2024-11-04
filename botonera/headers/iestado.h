ea2b69fc3eb8fd29dd71889dc773e79b8be53831
Mario Córdoba <cord0388@gmail.com>
2024-11-03 21:18:34 -0300

    matching estado with interface

--- botonera/headers/iestado.h
+++ botonera/headers/iestado.h
@@ -27,6 +27,7 @@ public:
     virtual void removeQek(QString boton) = 0;
     virtual void removeRange(QString boton) = 0;
     virtual void removeThreat(QString boton) = 0;
+    virtual void removeMIK(QString c) = 0;
 
     virtual QString getRange() = 0;
     virtual QString getLabel() = 0;
@@ -39,12 +40,12 @@ public:
     virtual QString getModos() = 0;
     virtual QString getOverlay() = 0;
     virtual QString getMIK() = 0;
-    virtual QString getLabelS()        = 0;
-    virtual QString getQEKS()          = 0;
-    virtual QString getCenterS()       = 0;
-    virtual QString getICMS()          = 0;
-    virtual QString getDisplayModeS()  = 0;
-    virtual QString getOverlayS()      = 0;
+    virtual QString getLabelS() = 0;
+    virtual QString getQEKS() = 0;
+    virtual QString getCenterS() = 0;
+    virtual QString getICMS() = 0;
+    virtual QString getDisplayModeS() = 0;
+    virtual QString getOverlayS() = 0;
 
 
     virtual void setRange(QString r) = 0;
@@ -55,12 +56,13 @@ public:
     virtual void setDisplayMode(QString d) = 0;
     virtual void setDisplaySelection(QString d) = 0;
     virtual void setICM(QString i) = 0;
-    virtual void setLabelS(QString l)      = 0;
-    virtual void setQEKS(QString q)        = 0;
-    virtual void setCenterS(QString c)     = 0;
-    virtual void setICMS(QString i)        = 0;
-    virtual void setDisplayModeS(QString d)= 0;
-    virtual void setOverlayS(QString o)    = 0;
+    virtual void setMIK(QString m) = 0;
+    virtual void setLabelS(QString l) = 0;
+    virtual void setQEKS(QString q) = 0;
+    virtual void setCenterS(QString c) = 0;
+    virtual void setICMS(QString i) = 0;
+    virtual void setDisplayModeS(QString d) = 0;
+    virtual void setOverlayS(QString o) = 0;
 
 };
 
